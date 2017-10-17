f92db14bc5ed3e4acf3a3d14a868b3c78cb43cda
Semjon <semjonglushkov@yandex.ru>
2017-10-17 18:49:49 +0300

    my final com

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from core.views import *
 
 urlpatterns = [
     url(r'^startpage/$', StartPage.as_view(), name="startpage"),
-    # url(r'profiles/', ProfielList.as_view(), name="profiles"),
-    # url(r'profile/id(?P<pk>\d+)/', ProfileMainDetail.as_view(), name="profile_main_page"),
+    url(r'profiles/', ProfileList.as_view(), name="profiles"),
+    url(r'profile/id(?P<pk>\d+)/', ProfileMainDetail.as_view(), name="profile_main_page"),
     # url(r'profile/id(?P<pk>\d+)/blogs/', ProfileBlogsDetail.as_view(), name="profile_blogs"),
     # url(r'profile/id(?P<pk>\d+)/posts/', ProfilePostsDetail.as_view(), name="profile_posts"),
     # url(r'profile/id(?P<pk>\d+)/comments/', ProfileCommentsDetail.as_view(), name="profile_main_page")
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from blog.models import *
 from comments.models import *
+from core.models import *
 # Create your views here.
 
 
@@ -11,7 +12,40 @@ class StartPage(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(StartPage, self).get_context_data(**kwargs)
-        context['Blogs'] = Blog.objects.order_by('created_at')[0:10]
-        context['Posts'] = Post.objects.order_by('created_at')[0:10]
+        context['Blogs'] = Blog.objects.order_by('-created_at')[0:10]
+        context['Posts'] = Post.objects.order_by('-created_at')[0:10]
         context['title'] = 'Start page'
+        context['user'] = self.request.user
         return context
+
+
+class ProfileList(ListView):
+    template_name = "profile_list.html"
+    context_object_name = "Users"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileList, self).get_context_data(**kwargs)
+        context['title'] = 'Profiles'
+        return context
+
+    def get_queryset(self):
+        queryset = User.objects.order_by('date_joined')
+        return queryset
+
+
+class ProfileMainDetail(DetailView):
+    template_name = "profile_main_page.html"
+    context_object_name = "user"
+    model = User
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileMainDetail, self).get_context_data(**kwargs)
+        context['title'] = 'Profile'
+        context['you'] = self.request.user
+        return context
+
+    def get_object(self):
+        obj = super(ProfileMainDetail, self).get_object()
+        if (obj.id != self.request.user.id):
+            raise PermissionError
+        return obj
